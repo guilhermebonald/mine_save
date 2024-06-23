90443568d27062a7f9243aeaab88c018e91d6c53
Guilherme Bonald <guilhermebonald@hotmail.com>
2024-06-23 18:15:52 -0300

    Menu changes

--- main.py
+++ main.py
@@ -25,24 +25,22 @@ print(lg)
 
 main_sync = main_sync.SyncMine(login=login, passw=password)
 
-while True:
-    print("1 - Salvar | 2 - Carregar | 3 - Sair")
-    option = int(input("-> "))
-    if option == 1:
-        main_sync.login()
-        main_sync.get_dir()
-        main_sync.upload()
-    elif option == 2:
-        main_sync.login()
-        main_sync.get_dir()
-        main_sync.download()
-    elif option == 3:
-        break
-    else:
-        print("Digite uma opção válida")
 
-
-# if os.name == "nt":
-#     print("For Windows here")
-# elif os.name == "posix":
-#     print("For Linux here")
+if os.name == "nt":
+    print("For Windows here")
+elif os.name == "posix":
+    while True:
+        print("1 - Salvar | 2 - Carregar | 3 - Sair")
+        option = int(input("-> "))
+        if option == 1:
+            main_sync.login()
+            main_sync.get_dir()
+            main_sync.upload()
+        elif option == 2:
+            main_sync.login()
+            main_sync.get_dir()
+            main_sync.download()
+        elif option == 3:
+            break
+        else:
+            print("Digite uma opção válida")
